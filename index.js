300f6244e10f53a838662bbbc62b1af7ef4ebc0d
Brijit Adak <brijitadak000@gmail.com>
2025-01-19 03:23:49 +0530

    add

---
+++ index.js
@@ -0,0 +1,41 @@
+async function gotLocation(position) {
+  let loading = true;
+  document.getElementById("locateBtn").innerHTML = "Loading...";
+  const { latitude, longitude } = position.coords;
+  const apiUrl = `http://localhost:3000/api/get-location?latitude=${latitude}&longitude=${longitude}`;
+  try {
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    const address = data.results[0].components;
+    document.getElementById("locateBtn").innerHTML = "Locate Me";
+    loading = false;
+    document.getElementById(
+      "continent"
+    ).innerHTML = `Continent : ${address.continent}`;
+    document.getElementById(
+      "country"
+    ).innerHTML = `Country : ${address.country}`;
+    document.getElementById("state").innerHTML = `State : ${address.state}`;
+    document.getElementById(
+      "district"
+    ).innerHTML = `District : ${address.state_district}`;
+    document.getElementById("city").innerHTML = `City : ${address.city}`;
+    document.getElementById(
+      "postalCode"
+    ).innerHTML = `Postal Code : ${address.postcode}`;
+    document.getElementById(
+      "fullAddress"
+    ).innerHTML = `Full Address : ${data.results[0].formatted}`;
+  } catch (error) {
+    document.getElementById("locateBtn").innerHTML = "Error";
+    console.log(error);
+  }
+}
+
+function errorLocation() {
+  console.log("Unable to retrieve location.");
+}
+
+locateBtn.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(gotLocation, errorLocation);
+});
---
+++ server.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch';  // ES6 import syntax for node-fetch
+import express from 'express';   // ES6 import syntax for express
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+app.use(cors());
+
+const apiKey = "your_api_key";
+
+app.get('/api/get-location', async (req, res) => {
+  const { latitude, longitude } = req.query;  // Get lat/long from client
+  const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${latitude},${longitude}&key=${apiKey}`;
+
+  try {
+    console.log(`Fetching location data...`);
+    
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log('Location data fetched successfully');
+    
+    res.json(data);  // Send the data back to the client
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching location data' });
+    console.error(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
